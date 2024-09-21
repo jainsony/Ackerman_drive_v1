8124792cebbcb4eeaacb5c31eb13da1f0e47dee5
Enigma bell <54368594+jainsony@users.noreply.github.com>
2024-09-21 22:25:59 +0530

    Everything is great and working fine

--- Arduino file/joystick_input_arduino_uno/joystick_input_arduino_uno.ino
+++ Arduino file/joystick_input_arduino_uno/joystick_input_arduino_uno.ino
@@ -4,6 +4,7 @@
 int VRY_PIN = A0; // Potentiometer VRX pin connected to A0 (analog pin)
 int VRX_PIN = A1; // Potentiometer VRY pin connected to digital pin D1
 int SW_PIN = A2;  // Switch connected to digital pin D2
+int EYE_POT = A3;
 
 Servo left_upper_lid;
 Servo left_lower_lid;
@@ -14,11 +15,13 @@ Servo y_axis_view;
 
 int vrxValue;
 int vryValue;
+int eyePotValue;
 
 int x_axis_write_value;
 int y_axis_write_value;
+int eye_pot_write_value;
 
-int upper_lid_write_value;
+int eye_lid_value;
 
 void setup() {
   // Initialize serial communication for debugging
@@ -36,6 +39,8 @@ void setup() {
 
   // Initialize the potentiometer pins (analog read pin doesn't need pinMode)
   pinMode(VRY_PIN, INPUT); // Digital pin for VRY
+  pinMode(VRX_PIN, INPUT);
+  pinMode(EYE_POT, INPUT);
 
   left_upper_lid.write(90);
   left_lower_lid.write(90);
@@ -49,21 +54,23 @@ void loop() {
   // Read the analog values from the potentiometers
   vrxValue = analogRead(VRX_PIN);  // Read VRX (A0 pin)
   vryValue = analogRead(VRY_PIN); // Read VRY (D1 pin, digital)
+  eyePotValue = analogRead(EYE_POT);
 
-  x_axis_write_value = map(vrxValue, 0, 1023, 30, 150);
+  x_axis_write_value = map(vrxValue, 0, 1023, 60, 120);
   y_axis_write_value = map(vryValue, 0, 1023, 55, 95);
-
-  upper_lid_write_value = map(y_axis_write_value,75, 95, 90, 150);
-  // lower_lid_write_value = map(y_axis_write_value,75, 95, 90, 150);
-
-  Serial.print("x_axis : ");
+  eye_lid_value = map(y_axis_write_value,75, 95, 90, 150);
+  eye_pot_write_value = map(eyePotValue, 0, 1023, 0, 40);
+ 
+  Serial.print("EYE_POT : ");
+  Serial.print(eye_pot_write_value);  
+  Serial.print(" | x_axis : ");
   Serial.print(x_axis_write_value);
   Serial.print(" | y_axis : ");
   Serial.print(y_axis_write_value);
   Serial.print(" | upper_lid : ");
-  Serial.print(upper_lid_write_value);
+  Serial.print(eye_lid_value);
   Serial.print(" | upper_lid_offset : ");
-  Serial.print(90 - (upper_lid_write_value - 90));
+  Serial.print(90 - (eye_lid_value - 90));
 
 
 
@@ -81,10 +88,10 @@ void loop() {
   if (swState == LOW) {
     Serial.println("Pressed");
     delay(20);
-    left_upper_lid.write(90);
-    left_lower_lid.write(90);
-    right_upper_lid.write(90);
-    right_lower_lid.write(90);
+    left_upper_lid.write(80);
+    left_lower_lid.write(100);
+    right_upper_lid.write(100);
+    right_lower_lid.write(80);
     delay(200);
 
   } else {
@@ -99,25 +106,28 @@ void loop() {
 
 void all_servo_write()
 {
-  if (y_axis_write_value > 75)
+  // UPPER EYE LID
+  if (y_axis_write_value > 75 and eye_lid_value > 120)
   {
-    left_upper_lid.write(upper_lid_write_value);
-    right_upper_lid.write(90 - (upper_lid_write_value - 90));
+    left_upper_lid.write(eye_lid_value);
+    right_upper_lid.write(90 - (eye_lid_value - 90));
   }
   else
   {
-    left_upper_lid.write(90);
-    right_upper_lid.write(90);
+    left_upper_lid.write(90 + eye_pot_write_value);
+    right_upper_lid.write(90 - eye_pot_write_value);
   }
-    if (y_axis_write_value < 75)
+
+  // LOWER EYE LID
+    if (y_axis_write_value < 75 and eye_lid_value < 60)
   {
-    left_lower_lid.write(upper_lid_write_value);
-    right_lower_lid.write(90 - (upper_lid_write_value - 90));
+    left_lower_lid.write(eye_lid_value);
+    right_lower_lid.write(90 - (eye_lid_value - 90));
   }
   else
   {
-    left_lower_lid.write(90);
-    right_lower_lid.write(90);
+    left_lower_lid.write(90 - eye_pot_write_value);
+    right_lower_lid.write(90 + eye_pot_write_value);
   }
 
   x_axis_view.write(x_axis_write_value);
